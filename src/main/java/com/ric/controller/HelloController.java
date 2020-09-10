ed1deca546499b976fd7a496a4c6653da20667a9
ricruan <1124317604@qq.com>
2020-09-10 10:46:58 +0800

    c

---
+++ src/main/java/com/ric/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ric.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(path = "/hello")
+    public String sayHello(){
+        System.out.println("hello world!");
+        return "success";
+    }
+}
